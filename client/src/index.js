a047a18efd505ed8ddf5e7dba9380a9a0d0e0809
Munsterberg <jetrussell93@gmail.com>
2017-12-15 16:28:07 -0500

    add flask cors and grab data for client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = async() => {
+    console.log('called');
+    const users = await axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`);
+    this.setState(() => { return { users: users.data.data.users }; });
+    console.log(users.data.data.users);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi there</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
