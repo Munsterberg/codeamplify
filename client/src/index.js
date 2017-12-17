8623c773fc8107b4b285a89cb456f8b3397cc93c
Munsterberg <jetrussell93@gmail.com>
2017-12-17 12:48:54 -0500

    setup react testing framework

--- client/src/index.js
+++ client/src/index.js
@@ -5,23 +5,21 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: []
-  };
+  }
 
   componentDidMount() {
     this.getUsers();
   }
 
   getUsers = async() => {
-    console.log('called');
-    const users = await axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`);
-    this.setState(() => { return { users: users.data.data.users }; });
-    console.log(users.data.data.users);
+    const response = await axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`);
+    this.setState(() => { return  { users: response.data.data.users }; });
   }
 
   render() {
     return (
       <div>
-        <h1>Hi there</h1>
+        hi there
       </div>
     );
   }
