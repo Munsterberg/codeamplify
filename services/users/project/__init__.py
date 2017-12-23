9829604c638e7ce7b97170d8e556f15270c7b91e
Munsterberg <jetrussell93@gmail.com>
2017-12-23 14:17:29 -0500

    import fix

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask.ext.bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 migrate = Migrate()
