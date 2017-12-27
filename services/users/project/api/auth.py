4adc8d367e3b51505fca81139f423b943e5880b3
Munsterberg <jetrussell93@gmail.com>
2017-12-27 11:53:59 -0500

    create auth register route

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -5,3 +5,40 @@ from project import db, bcrypt
 
 
 auth_blueprint = Blueprint('auth', __name__)
+
+
+@auth_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+    try:
+        user = User.query.filter(
+            or_(User.username == username, User.email == email)
+        ).first()
+        if not user:
+            new_user = User(
+                username=username,
+                email=email,
+                password=password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            auth_token = new_user.encode_auth_token(new_user.id)
+            response_object['status'] = 'success'
+            response_object['message'] = 'Registered user.'
+            response_object['auth_token'] = auth_token.decode()
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry, that user already exists.'
+            return jsonify(response_object), 400
+    except (exc.IntegrityError, ValueError) as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
